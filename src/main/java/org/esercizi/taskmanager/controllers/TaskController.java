03108352d4a8d2e6cbd7b98b46fe807f8f2d9a18
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-08-14 17:56:48 +0200

    lezione 3

--- src/main/java/org/esercizi/taskmanager/controllers/TaskController.java
+++ src/main/java/org/esercizi/taskmanager/controllers/TaskController.java
@@ -1,9 +1,11 @@
 package org.esercizi.taskmanager.controllers;
 
 
+import org.esercizi.taskmanager.dto.TaskResponse;
 import org.esercizi.taskmanager.models.Task;
 import org.esercizi.taskmanager.services.TaskService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,14 @@ public class TaskController {
     }
 
     @GetMapping
-    public List<Task>
+    public List<TaskResponse> findAll(
+            Authentication authentication
+    ) {
+        String username = authentication.getName();
+        List<Task> listTask = taskService.findAllByUsername(username);
+        return listTask.stream()
+                .map(
+                        task -> new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted())
+                ).toList();
+    }
 }
