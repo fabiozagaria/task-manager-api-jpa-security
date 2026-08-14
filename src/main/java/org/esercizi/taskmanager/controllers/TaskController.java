75ffcca896c0926e42ee311edf539c53dc2b49de
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-08-14 18:29:29 +0200

    lezione 3

--- src/main/java/org/esercizi/taskmanager/controllers/TaskController.java
+++ src/main/java/org/esercizi/taskmanager/controllers/TaskController.java
@@ -1,14 +1,15 @@
 package org.esercizi.taskmanager.controllers;
 
 
+import jakarta.validation.Valid;
+import org.esercizi.taskmanager.dto.TaskCreateRequest;
 import org.esercizi.taskmanager.dto.TaskResponse;
 import org.esercizi.taskmanager.models.Task;
 import org.esercizi.taskmanager.services.TaskService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +33,27 @@ public class TaskController {
                 .map(
                         task -> new TaskResponse(task.getId(), task.getTitle(), task.getDescription(), task.isCompleted())
                 ).toList();
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> save(
+            @Valid @RequestBody TaskCreateRequest taskCreateRequest,
+            Authentication authentication
+            )
+    {
+        String username = authentication.getName();
+        Task task = new Task();
+        task.setTitle(taskCreateRequest.title());
+        task.setDescription(taskCreateRequest.description());
+
+        Task createdTask = taskService.createTask(task, username);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new TaskResponse(
+                        createdTask.getId(),
+                        createdTask.getTitle(),
+                        createdTask.getDescription(),
+                        createdTask.isCompleted()));
     }
 }
